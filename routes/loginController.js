7405eb7f4d14ff61bceca4344151d08696762c93
Esteban <estecorral@gmail.com>
2020-02-06 21:28:46 +0100

    Correciones en el login

--- routes/loginController.js
+++ routes/loginController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 class LoginController {
     // GET /login
-    index(req, res, next) {
+    index(req, res) {
         res.locals.username = '';
         res.locals.email = '';
         res.locals.error = '';
@@ -24,11 +24,11 @@ class LoginController {
                 return;
             }
 
-            const tocken = jwt.sign({ _id: usuario._id }, process.env.JWT_SECRET, {
+            const token = jwt.sign({ _id: usuario._id }, process.env.JWT_SECRET, {
                 expiresIn: '1d'
             });
 
-            await res.json({success: true, tocken: tocken});
+            await res.json({success: true, session: {token: token, username: usuario.username, email: usuario.email }});
 
         } catch (e) {
             next(e);
